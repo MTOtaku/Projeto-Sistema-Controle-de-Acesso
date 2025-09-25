e74b803994df1a8898d4aa1353aef7ebc1922fa3
Mateus <mateusgameroficialbr@gmail.com>
2025-09-25 10:40:33 -0300

    Adicionado codigo do 19/09/2025

---
+++ Main.py
@@ -0,0 +1,123 @@
+# SCA - Sistema de Controle de Acesso
+# V1 - 19/09/2025 - Implementação inicial do projeto
+import sqlite3
+import cv2
+
+#
+# Inicialização
+#
+
+camera = cv2.VideoCapture(0)
+if not camera.isOpened():
+    print("Erro ao abrir a câmera")
+    exit()
+
+conexao = sqlite3.connect("sca.db")
+cursor = conexao.cursor()
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS usuarios(
+                   CPF VARCHAR(20) PRIMARY KEY,
+                   nome VARCHAR(255) NOT NULL,
+                   cartao VARCHAR (15) NOT NULL,
+                   foto VARCHAR(50),
+                   email VARCHAR(50) NOT NULL,
+                   telefone VARCHAR(20) NOT NULL)
+               """)
+conexao.commit()
+
+#
+# Cadastrar
+#
+def cadastrar():
+    print("\n=== Cadastro de Usuário ===")
+    cpf = input("CPF: ")
+    nome = input("Nome: ")
+    cartao = input("Cartão: ")
+    print("Pressione 'g' para salvar a foto.")
+
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            print("Erro ao capturar da câmera")
+            break
+
+        cv2.imshow("Câmera", frame)
+        tecla = cv2.waitKey(1)
+
+        if tecla == ord('g'):
+            filename = f"f{cpf}.png"
+            cv2.imwrite(filename, frame)
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+    foto = filename
+    email = input("Email: ")
+    telefone = input("Telefone: ")
+
+    try:
+        cursor.execute("""
+                       INSERT INTO usuarios (CPF, nome, cartao, foto, email, telefone)
+                       VALUES (?, ?, ?, ?, ?, ?)
+                       """, (cpf, nome, cartao, foto, email, telefone))
+        conexao.commit()
+        print("Usuário cadastrado com sucesso!")
+    except sqlite3.Error as e:
+        print("Ocorreu um erro: ", e)
+
+
+#
+# Buscar
+#
+def buscar():
+    print("=== Buscar por CPF ===")
+    cpf = input("Informe o CPF: ")
+    cursor.execute("SELECT * FROM usuarios WHERE CPF = ?", (cpf,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        print("CPF: ", resultado[0])
+        print("Nome: ", resultado[1])
+        print("Cartão: ", resultado[2])
+        print("Email: ", resultado[4])
+        print("Telefone: ", resultado[5])
+
+        if resultado[3]:
+            img = cv2.imread(resultado[3])
+            cv2.imshow(f"Foto de {resultado[1]}", img)
+            cv2.waitKey(2000)
+            cv2.destroyAllWindows()
+
+    else:
+        print("CPF não cadastrado!")
+
+
+#
+# Menu
+#
+def menu():
+    while True:
+        print("=== SCA - Sistema de Controle de Acesso")
+        print("1. Cadastrar")
+        print("2. Buscar")
+        print("6. Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            cadastrar()
+        if opcao == "2":
+            buscar()
+        elif opcao == "6":
+            print("Saindo do sistema")
+            conexao.close()
+            break
+        else:
+            print("Opção inválida")
+
+
+#
+# Programa principal
+#
+menu()
+
+
